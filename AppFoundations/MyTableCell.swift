881e0597cc0ee13e2f31fed052c5f383ab7e6727
Gabriel Henrique Daniel da Silva <ghds@10-48-48-26.0200.ufpe.br.iosda.org>
2018-06-20 12:02:32 -0300

    first commit

---
+++ AppFoundations/MyTableCell.swift
@@ -0,0 +1,24 @@
+//
+//  MyTableCell.swift
+//  AppFoundations
+//
+//  Created by Gabriel Henrique Daniel da Silva on 6/20/18.
+//  Copyright © 2018 Gabriel Henrique Daniel da Silva. All rights reserved.
+//
+
+import UIKit
+
+class MyTableCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ AppFoundations/Question.swift
@@ -0,0 +1,9 @@
+//
+//  Question.swift
+//  AppFoundations
+//
+//  Created by Gabriel Henrique Daniel da Silva on 6/20/18.
+//  Copyright © 2018 Gabriel Henrique Daniel da Silva. All rights reserved.
+//
+
+import Foundation
