a707356dc7d714087a75521b21b50dd5a8400401
Gabriel Henrique Daniel da Silva <ghds@10-48-48-26.0200.ufpe.br.iosda.org>
2018-06-20 10:44:05 -0300

    Initial Commit

---
+++ AppFoundations/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AppFoundations
+//
+//  Created by Gabriel Henrique Daniel da Silva on 6/20/18.
+//  Copyright © 2018 Gabriel Henrique Daniel da Silva. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
