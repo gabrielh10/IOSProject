9961a20ae5aa60d6b74e6cfcc34831e117ffd2bd
Gabriel Henrique Daniel da Silva <ghds@10-48-48-26.0200.ufpe.br.iosda.org>
2018-06-29 12:17:20 -0300

    Adding tab bar, but comunities and score need  some fix

--- AppFoundations/ViewController.swift
+++ AppFoundations/ViewController.swift
@@ -13,17 +13,17 @@ class ViewController: UIViewController, UITableViewDelegate,UITableViewDataSourc
     
     @IBOutlet weak var myTableView: UITableView!
     
-    var questions:[Question] = []
+   // var questions:[Question] = []
     
-     /*var questions:[Question]  = [Question(id: 1, question: "1How i do something ?", questionDetails: "Consider that i already try doing this like... "),
+     var questions:[Question]  = [Question(id: 1, question: "1How i do something ?", questionDetails: "Consider that i already try doing this like... "),
                  Question(id: 2, question: "2How i do something ?", questionDetails: "Consider that i already try... "),
                  Question(id: 3, question: "3How i do something ?", questionDetails: "Consider that i already try... "),
                  Question(id: 4, question: "4How i do something ?", questionDetails: "Consider that i already try... "),
                  Question(id: 5, question: "5How i do something ?", questionDetails: "Consider that i already try... "),
                  Question(id: 6, question: "6How i do something ?", questionDetails: "Consider that i already try... "),
                  Question(id: 7, question: "7How i do something ?", questionDetails: "Consider that i already try... ")]
-    */var answers:[Answer] = []
-    /*var answers:[Answer] = [Answer(id: 0, answer: "0You could do this and that....", score: 0, idQuestion: 1),
+ //   var answers:[Answer] = []
+    var answers:[Answer] = [Answer(id: 0, answer: "0You could do this and that....", score: 0, idQuestion: 1),
                             Answer(id: 1, answer: "0You should do this and that....", score: 0, idQuestion: 1),
                             Answer(id: 2, answer: "0You might do this and that....", score: 0, idQuestion: 1),
                             Answer(id: 3, answer: "0You might do this and that....", score: 0, idQuestion: 1),
@@ -43,21 +43,21 @@ class ViewController: UIViewController, UITableViewDelegate,UITableViewDataSourc
                             Answer(id: 17, answer: "4You should do this and that....", score: 0, idQuestion: 6),
                             Answer(id: 18, answer: "4You might do this and that....", score: 0, idQuestion: 6),
                             Answer(id: 19, answer: "4You might do this and that....", score: 0, idQuestion: 6)]
-    */
-    var knowledge:[Knowledge] = []
-    /*var knowledge:[Knowledge] = [Knowledge(id: 1, name: "Marketing"),
+    
+//    var knowledge:[Knowledge] = []
+     var knowledge:[Knowledge] = [Knowledge(id: 1, name: "Marketing"),
                                  Knowledge(id: 2, name: "Financeiro"),
                                  Knowledge(id: 3, name: "Admnistração"),
                                  Knowledge(id: 4, name: "RH"),
                                  Knowledge(id: 5, name: "Estratégia"),
                                  Knowledge(id: 6, name: "Capital")]
-    */
-    var profiles:[Profile] = []
-    var comunities:[Comunity] = []
-    /*var comunities:[Comunity] = [Comunity(id: 1, name: "Foundations"),
+    
+     var profiles:[Profile] = []
+ //   var comunities:[Comunity] = []
+     var comunities:[Comunity] = [Comunity(id: 1, name: "Foundations"),
                                  Comunity(id: 2, name: "CIN"),
                                  Comunity(id: 3, name: "UFPE")]
-    */
+    
     var start = 0
    
     override func viewDidLoad() {
@@ -73,7 +73,7 @@ class ViewController: UIViewController, UITableViewDelegate,UITableViewDataSourc
         }
         
         
-       /* profiles = [Profile(id: 1, name: "Allan", knowledge: [knowledge[0], knowledge[1]]),
+        profiles = [Profile(id: 1, name: "Allan", knowledge: [knowledge[0], knowledge[1]]),
                     Profile(id: 2, name: "César", knowledge: [knowledge[2], knowledge[4]]),
                     Profile(id: 3, name: "Gabriel", knowledge: [knowledge[1], knowledge[3]]),
                     Profile(id: 4, name: "Marina", knowledge: [knowledge[5], knowledge[0]])]
@@ -86,7 +86,7 @@ class ViewController: UIViewController, UITableViewDelegate,UITableViewDataSourc
         profiles[3].addComunity(comunity: comunities[0])
         profiles[3].addComunity(comunity: comunities[1])
         profiles[3].addComunity(comunity: comunities[2])
-        */
+        
         
         UsingDefaults.saveToDefaults(key: "questions", obj: questions)
         
@@ -110,12 +110,12 @@ class ViewController: UIViewController, UITableViewDelegate,UITableViewDataSourc
         }
         UsingDefaults.saveToDefaults(key: "profiles", obj: profiles)
         
-        var loggedUser:Profile = profiles[0]
+/*        var loggedUser:Profile = profiles[0]
         if let savedUser = UsingDefaults.loadFromDefaults(key: "loggedUser", objType: Profile.self) as? Profile{
             loggedUser = savedUser
         }
         UsingDefaults.saveToDefaults(key: "loggedUser", obj: loggedUser)
-        
+  */
         myTableView.delegate = self
         myTableView.dataSource = self
         // Do any additional setup after loading the view, typically from a nib.
