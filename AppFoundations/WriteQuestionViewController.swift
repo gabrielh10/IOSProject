fd58e300fc9e6e8444277ab5711c66f34e841304
Gabriel Henrique Daniel da Silva <ghds@10-48-48-26.0200.ufpe.br.iosda.org>
2018-07-03 10:03:18 -0300

    Fixing Tab Bar and adding navigator view controller (need some interface adjustments)

--- AppFoundations/WriteQuestionViewController.swift
+++ AppFoundations/WriteQuestionViewController.swift
@@ -17,7 +17,7 @@ class WriteQuestionViewController: UIViewController{
     
     override func viewDidLoad() {
         super.viewDidLoad()
-   
+        
         // Do any additional setup after loading the view.
     }
 
