829bc894731276c9ce7ecef9d24b09c2aacd1020
Gabriel Henrique Daniel da Silva <ghds@10-48-48-26.0200.ufpe.br.iosda.org>
2018-07-04 11:57:05 -0300

    Working

--- AppFoundations/ProfileViewController.swift
+++ AppFoundations/ProfileViewController.swift
@@ -8,7 +8,11 @@
 
 import UIKit
 
-class ProfileViewController: UIViewController {
+class ProfileViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+   
+    var loggedUser = Profile(id: 0, name: "", company: "", place: "", job: "", knowledge: [])
+//    var loggedUser : Profile
+    
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var company: UILabel!
     @IBOutlet weak var name: UILabel!
@@ -16,26 +20,49 @@ class ProfileViewController: UIViewController {
     @IBOutlet weak var numberOfAnswers: UILabel!
     @IBOutlet weak var numberOfComunities: UILabel!
     
+    @IBOutlet weak var profileComunitiesTableView: UITableView!
     override func viewDidLoad() {
+        profileComunitiesTableView.delegate = self
+        profileComunitiesTableView.dataSource = self
+        
         super.viewDidLoad()
         var profiles:[Profile] = []
+       
         
         if let savedProfiles = UsingDefaults.loadFromDefaults(key: "profiles", objType: [Profile].self) as? [Profile]{
             profiles = savedProfiles
         }
-        
-        var loggedUser:Profile = profiles[0]
+ 
         if let savedUser = UsingDefaults.loadFromDefaults(key: "loggedUser", objType: Profile.self) as? Profile{
             loggedUser = savedUser
+            print("äqui")
         }
+    
         UsingDefaults.saveToDefaults(key: "loggedUser", obj: loggedUser)
         // Do any additional setup after loading the view.
         name.text = loggedUser.name
         company.text = loggedUser.place
         job.text = loggedUser.job + " @ " + loggedUser.company
         numberOfComunities.text = String(loggedUser.comunities.count) + " Comunidades"
-        
+ 
     }
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        print(loggedUser.comunities.count)
+        return loggedUser.comunities.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        print("äqui")
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ProfileComunitiesCell", for: indexPath) as! ProfileComunitiesTableViewCell
+        cell.startupName.text = loggedUser.comunities[indexPath.row].name
+        cell.numberOfMembers.text = String(loggedUser.comunities[indexPath.row].quantity) + " Members"
+        cell.numberOfQuestions.text = String(0) + " Perguntas"
+        print("na table view", loggedUser.name)
+        return cell
+    }
+    
     
     override func viewDidAppear(_ animated: Bool) {
     //    name.text = loggedUser
